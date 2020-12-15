49dda6afa7bb01c6f39318c6a2ec7c4e8bc92c44
KarlTheCool <k@rlphilli.ps>
2020-12-14 19:18:00 -0800

    Basic nativefier template

---
+++ auto.js
@@ -0,0 +1,5 @@
+// Note: The javascript file is loaded after DOMContentLoaded, so you can assume the DOM is complete & available.
+
+(function() {
+    // TODO: hide #premiumservices
+})();
---
+++ build.bat
@@ -0,0 +1,1 @@
+nativefier --name "Countermail" --icon "icon.ico" --honest --tray --counter --bounce --inject "auto.js" "https://webmail.countermail.com"
