cc7963055202ecc512c3de81f26ba0123040bdb4
KarlTheCool <k@rlphilli.ps>
2020-12-22 03:17:37 -0800

    Added hider for premium services button

--- auto.js
+++ auto.js
@@ -1,5 +1,8 @@
 // Note: The javascript file is loaded after DOMContentLoaded, so you can assume the DOM is complete & available.
 
 (function() {
-    // TODO: hide #premiumservices
+    let premium = document.getElementById('premiumservices')
+    if (document.getElementById('premiumservices')) {
+        premium.style.display = 'none';
+    };
 })();
