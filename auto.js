748d90a4cf777cebe982ee823ce1ab313de222e5
KarlTheCool <k@rlphilli.ps>
2020-12-22 20:39:44 -0800

    Added Custom Frame and other cosmetic improvements

--- auto.js
+++ auto.js
@@ -1,8 +1,43 @@
 // Note: The javascript file is loaded after DOMContentLoaded, so you can assume the DOM is complete & available.
 
 (function() {
-    let premium = document.getElementById('premiumservices')
-    if (document.getElementById('premiumservices')) {
-        premium.style.display = 'none';
-    };
+    const remote = require('electron').remote;
+
+    // Add custom navbar
+    let topline = document.getElementById('topline')
+    if (topline) {
+        // Authenticated
+        topline.classList.add("taskbar");
+
+        topline.getElementsByClassName("topleft")[0].remove();
+
+        let taskbar__title = topline.getElementsByClassName("topright")[0];
+        taskbar__title.classList.remove('topright');
+        taskbar__title.classList.add('taskbar__title');
+
+        topline.appendChild(taskbar__close());
+    } else {
+        // Login Page
+        let taskbar = document.createElement("div");
+        taskbar.classList.add("taskbar");
+        taskbar.classList.add("taskbar--no-auth");
+
+        let taskbar__title = document.createElement("div");
+        taskbar__title.classList.add("taskbar__title");
+        taskbar__title.innerHTML = "Login";
+        taskbar.appendChild(taskbar__title);
+
+        taskbar.appendChild(taskbar__close());
+
+        document.body.appendChild(taskbar);
+    }
+
+    function taskbar__close() {
+        let exit = document.createElement("button");
+        exit.classList.add("taskbar__close");
+        exit.onclick = () => {
+            window.close();
+        }
+        return exit;
+    }
 })();
--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-nativefier --name "Countermail" --icon "icon.ico" --honest --tray --counter --hide-window-frame --bounce --inject "auto.js" "https://webmail.countermail.com"
+nativefier --name "CounterMail" --icon "icon.ico" --single-instance --honest --tray --counter --hide-window-frame --bounce --inject "auto.js" --inject "auto.css" "https://webmail.countermail.com"
