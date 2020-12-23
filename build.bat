966140de3ee99d084ae2599888218955bf0cbc04
KarlTheCool <k@rlphilli.ps>
2020-12-22 18:30:43 -0800

    comment

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-nativefier --name "Countermail" --icon "icon.ico" --honest --tray --counter --bounce --inject "auto.js" "https://webmail.countermail.com"
+nativefier --name "Countermail" --icon "icon.ico" --honest --tray --counter --hide-window-frame --bounce --inject "auto.js" "https://webmail.countermail.com"
