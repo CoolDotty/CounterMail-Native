9cddf295726362e30b4047711344b36dc9c9b16a
KarlTheCool <k@rlphilli.ps>
2020-12-27 20:53:37 -0800

    Added cred

--- auto.js
+++ auto.js
@@ -83,6 +83,9 @@
     // be triggered. The function will be called after it stops being called for
     // N milliseconds. If `immediate` is passed, trigger the function on the
     // leading edge, instead of the trailing.
+    //
+    // Yoinked from David Walsh: https://davidwalsh.name/javascript-debounce-function
+    // Who also yoinked it from Underscore.js: http://underscorejs.org/
     function debounce(func, wait, immediate) {
         var timeout;
         return function() {
